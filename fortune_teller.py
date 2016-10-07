cfe82ec854452891f84390fef2bd74e49a669760
luckeyme74 <jmurphy1364@mchenry.edu>
2016-10-07 18:20:24 -0500

    Add files via upload

--- fortune_teller.py
+++ fortune_teller.py
@@ -5,5 +5,19 @@ responses = ["Unfortunately, today is just not your day.", "Absolutely, and soon
 selection = random.choice(responses)
 answer = selection
 
-question = raw_input("Ask any question of the Omnipotent Olivia Onyx O'Reilly! ")
+question = raw_input("Ask any question of the Omnipotent Olivia Onyx O'Reilly! Learn the answers to the mysteries of the universe, or just find out where you left your car keys.\n")
 print answer
+question = raw_input("\nYour session includes two more questions. Enter another question, or enter no to end your fortune-telling session.\n")
+selection = random.choice(responses)
+result = selection
+if question != "no":
+    print result
+    question = raw_input("\nAsk your third and last question, or enter no to end your session.\n")
+    selection = random.choice(responses)
+    conclusion = selection
+    if question != "no":
+        print conclusion
+        print ("\nI hope you are pleased with the knowledge that you have been granted. Thank you for communing with the spirits. May your most heartfelt wishes come true!\n")
+    else:
+        print ("\nThank you for communing with the spirits. May all of your questions be answered, and all of your dreams come true!\n")
+else: print ("Thank you for communing with the spirits. May all of your questions be answered, and all of your dreams come true!")
